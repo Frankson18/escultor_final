c643b791d14fcb535d40dab040406e4cfc1e9124
Reilta <54174076+reilta@users.noreply.github.com>
2019-11-26 23:17:26 -0300

    Update tamanhoellipsoid.h

--- escultor_digital_app/tamanhoellipsoid.h
+++ escultor_digital_app/tamanhoellipsoid.h
@@ -3,6 +3,20 @@
 
 #include <QDialog>
 
+/**
+ * @brief The tamanhoEllipsoid class
+ * responsável pela caixa de diálogo aberta
+ * quando o usuário clica no putellipsoid e o
+ * usuário fornece os parâmetros do elipsoide
+ * as coordenadas x,y,z e o raio.
+ * @details
+ * getRaioX - pega o raio do elipsóide no eixo x.
+ * getRaioY - pega o raio do elipsóide no eixo y.
+ * getRaioZ - pega o raio do elipsóide no eixo z.
+ * getRaioE - tamanho do raio geral da elipsóide.
+ */
+
+
 namespace Ui {
 class tamanhoEllipsoid;
 }
