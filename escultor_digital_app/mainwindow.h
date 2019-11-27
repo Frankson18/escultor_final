ccd9268a1237233d5e59d73964deedd6032ae832
Reilta <54174076+reilta@users.noreply.github.com>
2019-11-26 23:40:53 -0300

    Update mainwindow.h

--- escultor_digital_app/mainwindow.h
+++ escultor_digital_app/mainwindow.h
@@ -12,6 +12,23 @@
 
 #include "QMessageBox"
 
+/**
+ * @brief The MainWindow class
+ * a classe responsável pela tela principal
+ * do projeto. Nela há as conexões devido as
+ * ações que podem acontecer com a interação
+ * do usuário. Possui as funções responsáveis
+ * pela interação e mudança dos parâmetros 
+ * fornecidos pelo usuário para o desenho das
+ * figuras geométricas.
+ * @details
+ * d - declaração da caixa de diálogo responsável pelo tamanho do scultor.
+ * dialog_pb - declaração da caixa de diálogo responsável pelo tamanho do box.
+ * dialog_ps - declaração da caixa de diálogo responsável pelo tamanho da esfera.
+ * dialog_pe - declaração da caixa de diálogo responsável pelo tamanho do elipsóide.
+ * dialog_pc - declaração da caixa de diálogo responsável pela definição da cor.
+ */
+
 namespace Ui {
 class MainWindow;
 }
